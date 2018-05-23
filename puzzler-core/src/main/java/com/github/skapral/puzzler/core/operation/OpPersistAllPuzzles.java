c9ebbc19926983d858ed2b7000a8c62e09fc93bf
Kapralov Sergey <skapralov@mail.ru>
2018-05-23 13:56:08 +0300

    Added prototype

---
+++ puzzler-core/src/main/java/com/github/skapral/puzzler/core/operation/OpPersistAllPuzzles.java
@@ -0,0 +1,44 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Kapralov Sergey
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+package com.github.skapral.puzzler.core.operation;
+
+import com.github.skapral.puzzler.core.IssueTracker;
+import com.github.skapral.puzzler.core.Operation;
+import com.github.skapral.puzzler.core.PuzzleSource;
+
+public class OpPersistAllPuzzles implements Operation {
+    private final PuzzleSource puzzleSource;
+    private final IssueTracker issueTracker;
+
+    public OpPersistAllPuzzles(PuzzleSource puzzleSource, IssueTracker issueTracker) {
+        this.puzzleSource = puzzleSource;
+        this.issueTracker = issueTracker;
+    }
+
+    @Override
+    public final void execute() {
+        puzzleSource.puzzles().forEach(issueTracker::persistPuzzle);
+    }
+}
