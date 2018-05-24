12c520dc9723d7b35b074e73611e62a5e1c6318a
Kapralov Sergey <skapralov@mail.ru>
2018-05-24 21:15:08 +0300

    #7: Implemented three-pars text.

--- puzzler-core/src/main/java/com/github/skapral/puzzler/core/text/threepars/TriggerWord.java
+++ puzzler-core/src/main/java/com/github/skapral/puzzler/core/text/threepars/TriggerWord.java
@@ -23,24 +23,16 @@
  *
  */
 
-/**
- * Three-paragraph approach for parsing text and preparing it for making puzzles.
- *
- * The text is splat to the paragraphs (non-empty pieces of text separated by newline).
- * Each paragraph is classified in the following way:
- *
- * <ul>
- *     <li>
- *         The paragraph which mentions the predefined trigger word (usually, the name
- *         of puzzlerbot user) is controlling paragraph: it is used to identify that
- *         the text is the puzzle and provide the metadata.
- *     </li>
- *     <li>
- *         First non-controlling paragraph in the text is the puzzle's title.
- *     </li>
- *     <li>
- *         The rest non-controlling paragraphs are combined into description.
- *     </li>
- * </ul>
- */
 package com.github.skapral.puzzler.core.text.threepars;
+
+/**
+ * Trigger word, which is used to distinct controlling paragraphs from the rest.
+ *
+ * @author Kapralov Sergey
+ */
+public interface TriggerWord {
+    /**
+     * @return the trigger word string.
+     */
+    String value();
+}
--- puzzler-core/src/main/java/com/github/skapral/puzzler/core/text/threepars/TwStatic.java
+++ puzzler-core/src/main/java/com/github/skapral/puzzler/core/text/threepars/TwStatic.java
@@ -23,24 +23,27 @@
  *
  */
 
-/**
- * Three-paragraph approach for parsing text and preparing it for making puzzles.
- *
- * The text is splat to the paragraphs (non-empty pieces of text separated by newline).
- * Each paragraph is classified in the following way:
- *
- * <ul>
- *     <li>
- *         The paragraph which mentions the predefined trigger word (usually, the name
- *         of puzzlerbot user) is controlling paragraph: it is used to identify that
- *         the text is the puzzle and provide the metadata.
- *     </li>
- *     <li>
- *         First non-controlling paragraph in the text is the puzzle's title.
- *     </li>
- *     <li>
- *         The rest non-controlling paragraphs are combined into description.
- *     </li>
- * </ul>
- */
 package com.github.skapral.puzzler.core.text.threepars;
+
+/**
+ * Static trigger word.
+ *
+ * @author Kapralov Sergey
+ */
+public class TwStatic implements TriggerWord {
+    private final String word;
+
+    /**
+     * Ctor.
+     *
+     * @param word The trigger word.
+     */
+    public TwStatic(String word) {
+        this.word = word;
+    }
+
+    @Override
+    public final String value() {
+        return word;
+    }
+}
