b5e4d21802b9eb338805823ee48d8d610b924e30
Kapralov Sergey <skapralov@mail.ru>
2019-01-11 23:25:43 +0300

    #90: Corrected license headers

--- puzzler-gitlab/src/main/java/com/github/skapral/puzzler/gitlab/source/PsrcFromGitlabEvent.java
+++ puzzler-gitlab/src/main/java/com/github/skapral/puzzler/gitlab/source/PsrcFromGitlabEvent.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) %today.year Kapralov Sergey
+ * Copyright (c) 2018 Kapralov Sergey
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -20,7 +20,6 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- *
  *
  */
 
